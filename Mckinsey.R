435b3cc0eb8117a525016e893bb6361ff2f91e86
litankumar <litankumar@gmail.com>
2015-06-01 17:01:16 +0530

    Vers1

---
+++ Mckinsey.R
@@ -0,0 +1,29 @@
+# Mckinsey digital labs Sales Data 
+
+# To calculate probability of an item appearing in a transaction 
+get.sales.items <- function(sales) {
+  sales.items <- grep('item', colnames(sales), value = TRUE) # Count the no of columns 
+  return(sales.items)
+}
+
+sales.items <- grep('item', colnames(total.sales), value = TRUE) 
+?nrow
+
+max(sales.data$id)
+
+summary(sales.data)
+
+sales.p<-data.frame(apply(sales.data[2:100000, 2:51], 2,sum) )
+
+sales.p<-sales.p/100000
+
+colnames(sales.p)<- "prob"
+length (sales.data)
+typeof(sales.p)
+
+hist(sales.p$prob)
+plot(sales.p$prob, rownames(sales.p))
+
+sales.p<-rbind(sales.items,sales.p)
+sales.p
+plot(sales.p)
